50cc3f80a2a2ac6292919ae2cf78439fe9fd1e63
Nic G <nicholasagammon@gmial.com>
2026-03-24 12:52:52 -0400

    added first string

--- pythonportion/datatypes.py
+++ pythonportion/datatypes.py
@@ -1,2 +1,14 @@
+import sys
+
+
 best_variable = 15
 print(best_variable)
+first_integer = 25
+print(first_integer)
+print(sys.getsizeof(first_integer))
+first_float = 3.1415
+print(first_float)
+
+first_string = "Hello, world!"
+print(first_string)
+print(first_string + "87")
